87142d0bca72e912da997bf6f3d374d982397d41
PierMael <pier-mael.brelivet@efrei.net>
2020-05-02 13:55:17 +0200

    vue friends list

---
+++ App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div>
+            <h1>Participants list ({{ participants.length }})</h1>
+            <ol>
+                <li v-for="person in participants" :key="person">
+                    {{ person.name }} {{ person.surname }}
+                </li>
+            </ol>
+        </div>
+        <h3>New participant</h3>
+        <form @submit.prevent="addNewParticipant()">
+            <label>Firstname</label>
+            <input type="text" v-model="newParticipant.name">
+            <label>Lastname</label>
+            <input type="text" v-model="newParticipant.surname">
+            <button>Add new participant</button>
+        </form>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                participants: [],
+                newParticipant: {},
+            };
+        },
+        methods: {
+            addNewParticipant() {
+                this.participants.push(this.newParticipant);
+                this.newParticipant = {};
+            }
+        }
+    };
+</script>
